584caa010ffcafbc58e6492d26886e35ff7eddb5
Salvador <salvacams@gmail.com>
2020-02-06 15:02:27 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'controlGastos-v0.1.01';
+var cacheName = 'controlGastos-v0.1.02';
 
 var filesToCache = [
   './',
@@ -41,8 +41,8 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
